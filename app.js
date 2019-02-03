57dc07cf53ff4863c2196878c6a762af60bc690f
YoheiMiyamoto <yoheimiyamoto@gmail.com>
2019-02-03 09:11:06 +0900

    handlerをfunctionに切り出してみる

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+function handler(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+}
+
+const server = http.createServer(handler);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
