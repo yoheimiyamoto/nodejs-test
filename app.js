a1fe36c8dc14c33f33ffc2da9a170d542963b818
yoheimiyamoto <yoheimiyamoto@r.recruit.co.jp>
2019-02-04 10:42:03 +0900

    add html

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 function handler(req, res) {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', (err, data) => {
+      if (err) throw err;
+      res.write(data)
+      res.end();
+    });
 }
 
 const server = http.createServer(handler);
